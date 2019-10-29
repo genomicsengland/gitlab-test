c85074a8a0cd7b7cbf1c875da5fe428897168fab
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-10-29 13:59:49 +0000

    change to get from db

--- test.r
+++ test.r
@@ -3,4 +3,17 @@ options(stringsAsFactors = FALSE,
 	scipen = 200)
 library(wrangleR)
 
-print(makegmc('110123123'))
+library(RPostgreSQL)
+drv <- dbDriver("PostgreSQL")
+p <- getprofile(
+				"mis_con"
+				  )
+con <- dbConnect(drv,
+             dbname = "gel_mi",
+             host     = p$host,
+             port     = p$port,
+             user     = p$user,
+             password = p$password)
+
+d <- dbGetQuery(con, 'select participant_id from cdm.vw_participant_level_data limit 10;')
+cat(d$participant_id[1])
