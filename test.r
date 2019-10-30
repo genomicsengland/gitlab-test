4b6876966eb73a58bac42930c32aa34e241131fc
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-10-30 17:35:45 +0000

    amend test script

--- test.r
+++ test.r
@@ -9,12 +9,14 @@ p <- getprofile(
 				"mis_con",
 				file = '.gel_config'
 				  )
-con <- dbConnect(drv,
-             dbname = "gel_mi",
-             host     = p$host,
-             port     = p$port,
-             user     = p$user,
-             password = p$password)
+#-- con <- dbConnect(drv,
+#--              dbname = "gel_mi",
+#--              host     = p$host,
+#--              port     = p$port,
+#--              user     = p$user,
+#--              password = p$password)
+#-- 
+#-- d <- dbGetQuery(con, 'select participant_id from cdm.vw_participant_level_data limit 10;')
+#-- cat(d$participant_id[1])
 
-d <- dbGetQuery(con, 'select participant_id from cdm.vw_participant_level_data limit 10;')
-cat(d$participant_id[1])
+cat(rnorm(1))
