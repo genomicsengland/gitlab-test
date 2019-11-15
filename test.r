20942bed842d500be7fdbccd596344edc06972ff
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-11-15 10:35:00 +0000

    add export

--- test.r
+++ test.r
@@ -16,5 +16,6 @@ con <- dbConnect(drv,
               user     = p$user,
               password = p$password)
  
-d <- dbGetQuery(con, 'select participant_id from consent_reading.participant limit 10;')
-cat(d$participant_id[1])
+d <- dbGetQuery(con, 'select participant_id from consent_reading.participant order by random() limit 10;')
+cat(d$participant_id[1]) 
+write.table(d, 'test-data.txt')
